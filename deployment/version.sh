2c82d67ce8092142521d21c30e7e4761f903b9fa
Mathieu Chauvin <mathieu.chauvin@orange.com>
2023-10-19 16:50:11 +0200

    toolbox back on dockerhub for missing tags....

--- deployment/version.sh
+++ deployment/version.sh
@@ -1,4 +1,4 @@
 while true; do
-  curl -w " - HTTP/%{http_version} %{response_code}\n" "http://localhost/toolbox/git/version"
+  curl -w " - HTTP/%{http_version} %{response_code}\n" "http://localhosti:8080/toolbox/git/version"
   sleep 1
 done
